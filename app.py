517cd751131d525b4480ac92fb5f0f1b527b3d90
unknown <phamminhkha.tc@gmail.com>
2022-12-28 21:20:04 +0700

    update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import database 
+from PyQt6.QtSql import QSqlTableModel, QSqlQueryModel
+import database
 import kautohelper
 import ldhelper
 import otphelper
@@ -10,42 +11,39 @@ import time
 IconHeart = './assets/images/heart.png'
 IconIsHeart = './assets/images/isHeart.png'
 
-#khởi động ứng dụng5
-db = database.Database();
-tableDevices = db.model("devices")
+# khởi động ứng dụng5
+db = database.Database()
+tableDevices = db.model("devies")
+
+# adb = kautohelper.AutoADB()
+# listDevice = adb.GetDevices()
+
+# def control_device(x):
+#   # Use subprocess to run adb commands for the specified device
+#   while True:
+#     # if(adb.FindImage(x, IconIsHeart) == False):
+#     #     if(adb.FindImage(x, IconHeart)):
+#     #         adb.ClickImage(x, IconHeart)
+#     adb.Swipe(x, 250, 600, 250, 300, 250)
+#     time.sleep(3)
 
 
-adb = kautohelper.AutoADB()
-listDevice = adb.GetDevices()
+# threads = []
+# for device_id in listDevice:
+#     print(device_id)
+#     t = threading.Thread(target=control_device, args=(device_id,))
+#     threads.append(t)
 
-def control_device(x):
-  # Use subprocess to run adb commands for the specified device
-  while True:
-    # if(adb.FindImage(x, IconIsHeart) == False):
-    #     if(adb.FindImage(x, IconHeart)):
-    #         adb.ClickImage(x, IconHeart)
-    adb.Swipe(x, 250, 600, 250, 300, 250)
-    time.sleep(3)
-    
+# for thread in threads:
+#   thread.start()
 
-
-threads = []
-for device_id in listDevice:
-    print(device_id)
-    t = threading.Thread(target=control_device, args=(device_id,))
-    threads.append(t)
-
-for thread in threads:
-  thread.start()
-
-for thread in threads:
-  thread.join()
-
+# for thread in threads:
+#   thread.join()
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
+    MainWindow = QtWidgets.QMainWindow()
     ui = Gui(tableDevices)
-    ui.setupUi(Dialog)
-    Dialog.show()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec())
--- database/__init__.py
+++ database/__init__.py
@@ -1,15 +1,19 @@
 from PyQt6 import QtCore, QtGui, QtWidgets, QtSql
-from PyQt6.QtSql import QSqlDatabase, QSqlQueryModel, QSqlQuery, QSqlResult 
+from PyQt6.QtSql import QSqlDatabase, QSqlQueryModel, QSqlQuery, QSqlResult
 import sys
+
+
 class Database:
     def __init__(self):
         self.db = QSqlDatabase.addDatabase("QSQLITE")
-        self.db.setDatabaseName("data.db")
+        self.db.setDatabaseName("./database/phonefarm.db")
         if not self.db.open():
             print("Failed to open database")
             sys.exit(1)
-       
+
     def model(self, table):
         projectModel = QSqlQueryModel()
-        projectModel.setQuery(("select * from {0}").format(table),self.db)
+        projectModel.setQuery("select * from devices")
+        # data = projectModel.query().exec()
+        print(projectModel.record(0).value("sonhiemvu"))
         return projectModel
